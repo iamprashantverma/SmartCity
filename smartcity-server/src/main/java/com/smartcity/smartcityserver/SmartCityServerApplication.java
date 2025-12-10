071d520660ddbdc68e79052a45fec063c1b2dc4f
iamprashantverma <prashant2k26@gmail.com>
2025-12-10 23:31:04 +0530

    chore: folder restructured

--- smartcity-server/src/main/java/com/smartcity/smartcityserver/SmartCityServerApplication.java
+++ smartcity-server/src/main/java/com/smartcity/smartcityserver/SmartCityServerApplication.java
@@ -1,14 +1,16 @@
 package com.smartcity.smartcityserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@Slf4j
 public class SmartCityServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCityServerApplication.class, args);
+		log.info("Smart City Application is Running !");
 	}
 
 }
