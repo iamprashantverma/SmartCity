e9cb3d396bd26f9cbd2588b1a70736b36f0ca000
iamprashantverma <prashant2k26@gmail.com>
2025-12-11 00:36:29 +0530

    feat: add AuthController with initial authentication endpoints

---
+++ smartcity-server/src/main/java/com/smartcity/smartcityserver/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.smartcity.smartcityserver.controller;
+
+import com.smartcity.smartcityserver.dto.LoginRequestDTO;
+import com.smartcity.smartcityserver.dto.LoginResponseDTO;
+import com.smartcity.smartcityserver.dto.UserCreateDTO;
+import com.smartcity.smartcityserver.dto.UserResponseDTO;
+import com.smartcity.smartcityserver.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginDTO){
+        LoginResponseDTO loginResponseDTO = authService.login(loginDTO);
+
+        return ResponseEntity.ok(loginResponseDTO);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponseDTO> singUp(@Valid @RequestBody UserCreateDTO studentCreateDTO) {
+        UserResponseDTO userCreated = authService.signUp(studentCreateDTO);
+
+        return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+    }
+
+
+
+
+}
